f5b1572e904b26fc6803fc9ca290b838c6cfbaee
tommycroot <tommycroot@rocketmail.com>
2021-08-08 15:02:06 +0100

    add netlify to CORS origins

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://snippetmanager.netlify.app"],
     credentials: true,
   })
 );
