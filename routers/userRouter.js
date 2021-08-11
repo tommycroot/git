bd89889b9b2e05cebc39603d4cc0edff80203de4
tommycroot <tommycroot@rocketmail.com>
2021-08-11 18:41:01 +0100

    fixing cookie clear error

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,57 +3,54 @@ const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-
 router.post("/", async (req, res) => {
   try {
     const { email, password, passwordVerify } = req.body;
 
     // validation
 
-    if(! email || !password || !passwordVerify)
+    if (!email || !password || !passwordVerify)
       return res.status(400).json({
         errorMessage: "Please enter all required fields.",
       });
 
     if (password.length < 6)
       return res.status(400).json({
-          errorMessage: "Please enter a password of at least 6 characters.",
+        errorMessage: "Please enter a password of at least 6 characters.",
       });
 
     if (password !== passwordVerify)
       return res.status(400).json({
-          errorMessage: "Please enter the same password twice for verification",
+        errorMessage: "Please enter the same twice for verification.",
       });
-// no account exists for this email
+
+    // make sure no account exists for this email
 
     const existingUser = await User.findOne({ email });
     if (existingUser)
       return res.status(400).json({
-          errorMessage: "An account with this e-mail already exists.",
+        errorMessage: "An account with this email already exists.",
       });
 
-// hash the password
+    // hash the password
 
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
 
-  // save the user in the myFirstDatabase
+    // save the user in the database
 
-    const newUser = new User ({
+    const newUser = new User({
       email,
       passwordHash,
     });
 
     const savedUser = await newUser.save();
 
-//    res.send(savedUser);
-
-
     // create a JWT token
 
     const token = jwt.sign(
       {
-        id: savedUser._id
+        id: savedUser._id,
       },
       process.env.JWT_SECRET
     );
@@ -70,42 +67,40 @@ router.post("/", async (req, res) => {
             ? false
             : process.env.NODE_ENV === "production" && true,
       })
-        .send();
+      .send();
+  } catch (err) {
+    res.status(500).send();
+  }
+});
 
-  } catch(err) {
-        res.status(500).send();
-      }
-    });
-
-router.post("/login", async (req,res) => {
+router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
 
     // validation
 
-    if(!email || !password)
+    if (!email || !password)
       return res.status(400).json({
         errorMessage: "Please enter all required fields.",
       });
 
-
-  // get user account
+    // get user account
 
     const existingUser = await User.findOne({ email });
     if (!existingUser)
       return res.status(401).json({
-          errorMessage: "You have entered the wrong e-mail or password.",
+        errorMessage: "Wrong email or password.",
       });
 
-      const correctPassword = await bcrypt.compare(
-        password,
-        existingUser.passwordHash
-      );
+    const correctPassword = await bcrypt.compare(
+      password,
+      existingUser.passwordHash
+    );
 
-      if (!correctPassword)
-        return res.status(401).json({
-          errorMessage: "You have entered the wrong e-mail or passsword",
-        });
+    if (!correctPassword)
+      return res.status(401).json({
+        errorMessage: "Wrong email or password.",
+      });
 
     // create a JWT token
 
@@ -128,25 +123,23 @@ router.post("/login", async (req,res) => {
             ? false
             : process.env.NODE_ENV === "production" && true,
       })
-        .send();
-
-  } catch(err) {
-        res.status(500).send();
-      }
+      .send();
+  } catch (err) {
+    res.status(500).send();
+  }
 });
 
-
 router.get("/loggedIn", (req, res) => {
-    try {
-      const token = req.cookies.token;
+  try {
+    const token = req.cookies.token;
 
-      if (!token) return res.json(null);
+    if (!token) return res.json(null);
 
-      const validatedUser = jwt.verify(token, process.env.JWT_SECRET);
+    const validatedUser = jwt.verify(token, process.env.JWT_SECRET);
 
-      res.json(validatedUser.id);
-    } catch (err) {
-      return res.json(null);
+    res.json(validatedUser.id);
+  } catch (err) {
+    return res.json(null);
   }
 });
 
